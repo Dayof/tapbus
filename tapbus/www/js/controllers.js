1b61110f096ca7500061db823526b871c4c7ed75
Day <dayannefernandesc@gmail.com>
2016-05-06 03:03:31 -0300

    Adicionando o ngCordova ao projeto

---
+++ tapbus/www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('app.controllers', [])
+  
+.controller('tapBusCtrl', function($scope) {
+    $scope.data = {}
+
+})
+   
+.controller('mapCtrl', function($scope) {
+    $scope.data = {}
+})
+   
+.controller('placesNearbyCtrl', function($scope) {
+    $scope.data = {}
+})
+    
