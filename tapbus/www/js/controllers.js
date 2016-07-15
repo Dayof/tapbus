0ddc2135c47ce58802d77a758c5abfbdb089f089
Day <dayannefernandesc@gmail.com>
2016-07-15 11:16:31 -0300

    lib of places from google api

--- tapbus/www/js/controllers.js
+++ tapbus/www/js/controllers.js
@@ -4,98 +4,100 @@ angular.module('app.controllers', [])
     $scope.data = {};
 })
 
-.controller('mapCtrl', function($scope, $rootScope, $filter, $state, $ionicLoading, nfcService) {
+.controller('mapCtrl', function($scope,
+                                $rootScope,
+                                $filter,
+                                $state,
+                                $ionicLoading,
+                                nfcService) {
 
-    $scope.data = {};
-    //$scope.latLngList = [];
-    //
     $scope.tag = nfcService.tag;
 
-    // $scope.setLat = function(input){
-    //   $scope.latLngList = $filter('testf')(input);
-    // };
-
-  //   google.maps.event.addDomListener(window, 'load', function() {
-  //      var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
-   //
-  //      var mapOptions = {
-  //          center: myLatlng,
-  //          zoom: 16,
-  //          mapTypeId: google.maps.MapTypeId.ROADMAP
-  //      };
-   //
-  //      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-   //
-  //      navigator.geolocation.getCurrentPosition(function(pos) {
-  //          map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-  //          var myLocation = new google.maps.Marker({
-  //              position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-  //              map: map,
-  //              title: "My Location"
-  //          });
-  //      });
-   //
-  //      $scope.map = map;
-  //  });
-
-    // google.maps.event.addDomListener(window, 'load', function() {
-    //   $ionicLoading.show();
-    //     var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
-    //
-    //     var mapOptions = {
-    //         center: myLatlng,
-    //         zoom: 16,
-    //         mapTypeId: google.maps.MapTypeId.ROADMAP
-    //     };
-    //
-    //     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    //
-    //     navigator.geolocation.getCurrentPosition(function(pos) {
-    //         map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-    //
-    //         var myLocation = new google.maps.Marker({
-    //             position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-    //             map: map,
-    //             title: "My Location"
-    //         });
-    //     });
-    //
-    //     $scope.map = map;
-    //
-    //     //  $ionicLoading.hide();
-    // });
-    $scope.lat = 10.3000;
-
     var listener = $rootScope.$on('new.tag', function(event, tag) {
-      updateMarkers();
+      $ionicLoading.show();
+      updateMarkers(tag);
     });
 
     $scope.$on('$destroy', function(event) {
       listener();
     });
 
-    function updateMarkers() {
-      var latLng2 = new   google.maps.LatLng($scope.lat, -100.4833);
-      console.log($scope.lat);
-      $scope.lat = $scope.lat + 30.3000;
+    function updateMarkers(tag) {
+      var infowindow;
+      var service;
+      var latLngList = $filter('testf')(tag.ndefMessage[0]);
+      var newLatLng = new google.maps.LatLng(parseFloat(latLngList[0]),
+                                          parseFloat(latLngList[1]));
+
+      console.log(latLngList);
+      console.log(newLatLng);
 
       var markerNfc = new google.maps.Marker({
           map: $scope.map,
           animation: google.maps.Animation.DROP,
-          position: latLng2
+          position: newLatLng
       });
 
-      $scope.map.setCenter(latLng2);
+      $scope.map.setCenter(newLatLng);
       markerNfc.setMap($scope.map);
+      $ionicLoading.hide();
+
+      var request = {
+         location: newLatLng,
+         radius: 2500,
+         types: ['bus_station']
+       };
+
+       infowindow = new google.maps.InfoWindow();
+       service = new google.maps.places.PlacesService($scope.map);
+       service.search(request, callback);
+
+       function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+
+        function createMarker(place) {
+
+          var placeLoc = place.geometry.location;
+
+          var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: place.geometry.location
+          });
+          var content='<strong style="font-size:1.2em">'+place.name+'</strong>'+
+                      '<br/><strong>Latitude:</strong>'+placeLoc.lat()+
+                      '<br/><strong>Longitude:</strong>'+placeLoc.lng();
+
+          //make a request for further details
+          service.getDetails({reference:place.reference}, function (place, status)
+          {
+            if (status == google.maps.places.PlacesServiceStatus.OK)
+            {
+              more_content='<hr/><strong><a href="'+place.url+'" target="details">Details</a>';
+
+              if(place.website)
+              {
+                more_content+='<br/><br/><strong><a href="'+place.website+'" target="details">'+place.website+'</a>';
+              }
+            }
+            google.maps.event.addListener(marker, 'click', function() {
+             infowindow.setContent(content);
+             infowindow.open($scope.map, this);
+            });
+          });
+        }
+      }
     }
 
     var latLng = new google.maps.LatLng(37.3000, -120.4833);
-    //var latLng = new google.maps.LatLng(parseFloat(latLngList[0]), parseFloat(latLngList[1]));
-    //alert(latLng);
+
     var mapOptions = {
         center: latLng,
         zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.HYBRID
     };
 
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
